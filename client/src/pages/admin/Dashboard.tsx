87dd20773a3069cab12cbedb1fc034222def9d0c
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2026-01-21 11:18:27 +0530

    added active show component in admin dasboard

--- client/src/pages/admin/Dashboard.tsx
+++ client/src/pages/admin/Dashboard.tsx
@@ -2,11 +2,13 @@ import { dummyDashboardData } from "@/assets/assets";
 import Title from "@/components/admin/Title";
 import BlurCircle from "@/components/BlurCircle";
 import Loading from "@/components/Loading";
+import { dateFormat } from "@/lib/DateFormat";
 import type { DashboardData } from "@/types/assets";
 import {
   ChartLineIcon,
   CircleDollarSignIcon,
   PlayCircleIcon,
+  StarIcon,
   UserIcon,
 } from "lucide-react";
 import { useEffect, useState } from "react";
@@ -77,6 +79,35 @@ const Dashboard = () => {
             </div>
           ))}
         </div>
+      </div>
+      <p className="mt-10 text-lg font-medium">Active Shows</p>
+      <div className="relative flex flex-wrap gap-6 mt-4 max-w-5xl">
+        <BlurCircle top="-100px" left="-10%" />
+        {dashboarddata.activeShows.map((show) => (
+          <div
+            className="w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300"
+            key={show._id}
+          >
+            <img
+              src={show.movie.poster_path}
+              alt=""
+              className="h-60 w-full object-cover"
+            />
+            <p className="font-medium p-2 truncate">{show.movie.title}</p>
+            <div className="flex items-center justify-between px-2">
+              <p className="text-lg font-medium">
+                {currency} {show.showPrice}
+              </p>
+              <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+                <StarIcon className="w-4 h-4 text-primary fill-primary" />
+                {show.movie.vote_average.toFixed(1)}
+              </p>
+            </div>
+            <p className="px-2 pt-2 text-sm text-gray-500">
+              {dateFormat(show.showDateTime)}
+            </p>
+          </div>
+        ))}
       </div>
     </>
   ) : (
