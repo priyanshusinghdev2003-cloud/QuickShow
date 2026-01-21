380b8201cd83f004a8bc4ebfcf1e3987ce51ac59
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2026-01-21 11:32:49 +0530

    implemented list show page in admin route

--- client/src/pages/admin/ListShows.tsx
+++ client/src/pages/admin/ListShows.tsx
@@ -1,5 +1,86 @@
+import { dummyShowsData } from "@/assets/assets";
+import Title from "@/components/admin/Title";
+import Loading from "@/components/Loading";
+import { dateFormat } from "@/lib/DateFormat";
+import type { Show } from "@/types/assets";
+import { useEffect, useState } from "react";
+
+interface shows {
+  movie: Show;
+  showDateTime: string;
+  showPrice: number;
+  occupiedSeats: {
+    [key: string]: string;
+  };
+}
+
 const ListShows = () => {
-  return <div>ListShows</div>;
+  const currency = import.meta.env.VITE_CURRENCY as string;
+  const [shows, setShows] = useState<shows[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const getAllShows = async () => {
+    try {
+      setShows([
+        {
+          movie: dummyShowsData[0],
+          showDateTime: "2025-06-30T02:30:00.000z",
+          showPrice: 59,
+          occupiedSeats: {
+            A1: "user_1",
+            B1: "user_2",
+            C1: "user_3",
+          },
+        },
+      ]);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAllShows();
+  }, []);
+
+  return !loading ? (
+    <>
+      <Title text1="List" text2="Shows" />
+      <div className="max-w-4xl mt-6 overflow-x-auto">
+        <table className="w-full border-collapse rounded-md overflow-hidden text-nowrap">
+          <thead>
+            <tr className="bg-primary/20 text-left text-white">
+              <th className="p-2 font-medium pl-5">Movie Name</th>
+              <th className="p-2 font-medium ">Show Time</th>
+              <th className="p-2 font-medium ">Total Bookings</th>
+              <th className="p-2 font-medium ">Earnings</th>
+            </tr>
+          </thead>
+          <tbody className="text-sm font-light">
+            {shows.map((show, idx) => (
+              <tr
+                key={idx}
+                className="border-b border-primary/10 bg-primary/5 even:bg-primary/10"
+              >
+                <td className="p-2 pl-5 min-w-45">{show.movie.title}</td>
+                <td className="p-2">{dateFormat(show.showDateTime)}</td>
+                <td className="p-2">
+                  {Object.keys(show.occupiedSeats).length}
+                </td>
+                <td className="p-2">
+                  {currency}
+                  {Object.keys(show.occupiedSeats).length * show.showPrice}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
 };
 
 export default ListShows;
