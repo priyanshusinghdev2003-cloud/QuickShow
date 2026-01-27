cd0ea9041d962b148c779d2b49a27d07665a1209
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2026-01-27 18:27:59 +0530

    initialize server with Express, Clerk, Inngest, and essential middleware and routes.

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ import bookingRouter from "./routes/booking.route.js";
 import adminRouter from "./routes/admin.routes.js";
 import userRouter from "./routes/user.routes.js";
 import { stripeWebhooks } from "./controllers/stripe.webhooks.js";
+import helmet from "helmet";
+import ratelimit from "express-rate-limit";
 
 const app = express();
 
@@ -20,13 +22,26 @@ app.post(
 );
 
 //Middleware
+app.use(helmet());
+
 app.use(clerkMiddleware());
 app.use(cors());
 app.use(express.json());
 
+//Rate Limiting
+const limiter = ratelimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+
+app.use(limiter);
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/show", showRouter);
 app.use("/api/booking", bookingRouter);
